db52b6ad36c3721b007ca2f5fe0669228bbf5ab1
Emma <emmasommer2612@gmail.com>
2022-07-01 22:50:40 +0200

    Initializing project

---
+++ src/main/java/tenniscourts/TennisCourtsApplication.java
@@ -0,0 +1,13 @@
+package tenniscourts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisCourtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisCourtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tenniscourts/TennisCourtsApplicationTests.java
@@ -0,0 +1,13 @@
+package tenniscourts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisCourtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
